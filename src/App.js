242de85452d8834a8d953f4dfe178c0167dddcd8
adsa88 <adiltoncss@gmail.com>
2021-01-15 08:09:01 -0300

    reprçao do conceitos de imut.Start novos loaders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
+
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  //useState retorna um array com 2 posições
+  // 1. Variável com o seu valor inicial e 
+  // 2. Função para atualizarmos esse valor
+
+  function handleAddProject(){
+    // projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+  }
+
   return (
     <>
-    <Header/>
-    <Header/>
+    {/* <Header title="Working Sample"/>   */}
+    {/* Ele só fecha, sem abrir(?) */}
+    <Header title="Projects">
+    <ul>
+      {projects.map(project => <li key={project}>{project}</li>)}
+    </ul>
+    <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </Header>  
     </>
-    
-    ); // Ele só fecha, sem abrir(?)
+    ); 
 }
 
 export default App;
