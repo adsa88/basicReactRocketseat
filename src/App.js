8d7d288f06d5839a2705b1a69a6cbca84e527d6f
adsa88 <adiltoncss@gmail.com>
2021-01-15 06:14:41 -0300

    compartimentaçao e framentos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Header/>
+    </>
+    
+    ); // Ele só fecha, sem abrir(?)
+}
+
+export default App;
